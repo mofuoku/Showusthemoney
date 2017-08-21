890ac287c7de14e66a3753db227368ab40a6f955
Insuk <jdebreac@gmail.com>
2017-08-21 09:34:39 -0400

    plots add

--- Insuk/Mixed_Effect_Model.R
+++ Insuk/Mixed_Effect_Model.R
@@ -1,4 +1,3 @@
-
 library(data.table)
 library(MASS)
 library(stats)
@@ -18,7 +17,9 @@ colnames(Data02)[colSums(is.na(Data02)) > 0]
 #################### Test with Random 1000 rows #########################
 ##Data05 <- Data02[1:1000,]
 
-Data05 <- Data02 %>% sample_n(1000)
+Data05 <- Data02 %>% sample_n(10000)
+
+Data03 <- Data02 %>% filter(logerror>0.5,logerror<-0.5) %>% sample_n(100)
 Data06 <- Data02 %>% sample_n(1000)
 View(Data06)
 View(Data05)
@@ -26,20 +27,41 @@ Data05<-Data05[, !names(Data05) %in% c("hashottuborspa","taxdelinquencyflag","fi
 
 names(Data05)
 
-
+#[1] "parcelid"                     "logerror"                    
+#[3] "lotsizesquarefeet"            "structuretaxvaluedollarcnt"  
+#[5] "taxvaluedollarcnt"            "landtaxvaluedollarcnt"       
+#[7] "taxamount"                    "calculatedfinishedsquarefeet"
+#[9] "month"                        "hashottuborspa"              
+#[11] "latitude"                     "longitude"                   
+#[13] "propertylandusetypeid"        "regionidcity"                
+#[15] "regionidcounty"               "regionidzip"                 
+#[17] "roomcnt"                      "yearbuilt"                   
+#[19] "taxdelinquencyflag"           "rawcensustractandblock"      
+#[21] "bathroomcnt"                  "censustractandblock"         
+#[23] "bedroomcnt"                   "fireplaceflag"  
 ?lme
 
 model.predict<-lme(logerror~taxamount*(structuretaxvaluedollarcnt+lotsizesquarefeet+taxvaluedollarcnt), random=~1|taxamount/structuretaxvaluedollarcnt/lotsizesquarefeet/taxvaluedollarcnt, data=Data05, method="ML")
 
-#model.predict<-lmer(logerror~(taxamount)*((1|structuretaxvaluedollarcnt)+(1|landtaxvaluedollarcnt)+
-#                                           taxvaluedollarcnt+lotsizesquarefeet+bedroomcnt+propertylandusetypeid+roomcnt), data=Data05)
+model.predict<-lme(logerror~taxamount*(structuretaxvaluedollarcnt+lotsizesquarefeet+taxvaluedollarcnt+landtaxvaluedollarcnt), random=~1|taxamount/structuretaxvaluedollarcnt/lotsizesquarefeet/taxvaluedollarcnt/landtaxvaluedollarcnt, data=Data05, method="ML")
+
 predict01=predict(model.predict, Data05, interval = "prediction") #Construct prediction invervals
 predict01<-as.data.frame(predict01)
-result=as.data.table(cbind(Data06[,1:2],'fit'=predict01[,1]))
+result=as.data.table(cbind(Data05[,1:2],'fit'=predict01[,1]))
 
 
 model01<-lm(fit~logerror,data=result)
 summary(model01)
+
+summary(model01)[[4]]
+
+pdf('./Showusthemoney/Insuk/Result_MEM_10000.pdf') 
+plot(result$logerror,result$fit,ylim=c(-1,1),xlim=c(-1,1),ylab="Predicted logerror", xlab="logerror",
+     main = "Random Sample 10000: Predicted Logerror Vs. Logerror")
+abline(0,1,col="red")
+abline(0.1,1,col="red",lty=2)
+abline(-0.1,1,col="red",lty=2)
+abline(model01)
 
 se.lines<-function(model){
   b1<-coef(model)[2]+summary(model)[[4]][4]
@@ -52,15 +74,8 @@ se.lines<-function(model){
   abline(a1,b1,lty=2)
   abline(a2,b2,lty=2)
 }
-
-
-pdf('Result_MEM_04.pdf') 
-plot(result$logerror,result$fit,ylim=c(-1,1),xlim=c(-1,1),ylab="Predicted logerror", xlab="logerror")
-abline(0,1,col="red")
-abline(0.1,1,col="red",lty=2)
-abline(-0.1,1,col="red",lty=2)
-abline(model01)
 se.lines(model01)
+
 
 dev.off()
 
